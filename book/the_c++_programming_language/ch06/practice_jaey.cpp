622dc6889456f750d1d68a658aff4a363054cf73
jaeyp <jp.inseoul@gmail.com>
2019-06-22 07:30:00 +0000

    moved definition of class member functions outside

--- book/the_c++_programming_language/ch06/practice_jaey.cpp
+++ book/the_c++_programming_language/ch06/practice_jaey.cpp
@@ -56,9 +56,16 @@ struct PackedBar {
 };
 #pragma pack(8)	// restore machine word align
 
-//TODO: mutable and const_cast
-
 class Chapter6 {
+public:
+	void TestSign(void);
+	void TestAlignment(void);
+	void TestAlignment2(void);
+	void TestAlignment3(void);
+	//void TestStringLiteral(void);
+	void TestConst(void);
+	void TestLRvalue(void);
+
 private:
 	int x {111};
 	struct Empty {};
@@ -67,208 +74,213 @@ private:
 	struct AlignedBar bar2 {};
 	struct PackedBar bar3 {};
 
-	// 'const' return value in function
-	const char *func_with_const_return()
-	{ return "Some key"; }
-
-	// 'const' member function
-	void func_as_const_member() const
-	{
-		// x = 1	// error
-		std::cout << x << "\n";
-	}
-
-public:
-	/* signed and unsigned */
-	void TestSign(void)
-	{
-		std::cout << "\n* Signed and Unsigned Characters *\n";
-		// 100 == 0110 0100, 1's complement: 1001 1011, 2's comlement: 1001 1100
-		// then, -100 == 1001 1100
-		signed char sc = -100;	
-		unsigned char uc = sc;	// 1001 1100 == 156 (unsigned), -100 (signed)
-		std::cout << 
-			"sc: " << (int)sc << "\n"
-			"uc: " << (int)uc << "\n";
-
-		char count[256] = {0,};
-		char *cp = count;
-		std::cout << "address of count[0]: " << &(count) << "\n";
-		std::cout << "address of count[-100]: " << (void*)(cp-100) << "\n";
-		//std::cout << (int)*(cp-120) << "\n";
-		//std::cout << (int)*(cp+136) << "\n";
-		std::cout << "count[-100]: " << (int)count[-100] << "\n";
-		std::cout << "count[156]: " << (int)count[156] << "\n";
-		std::cout << "increasement of count[-100]: " << ""; 
-		++count[sc];	// undefined behavior
-		std::cout << (int)*(cp-100) << " (undefined behavior)\n";
-		std::cout << "increasement of count[156]: " << "";
-		++count[uc];
-		std::cout << (int)*(cp+156) << "\n";
-
-		for(int i; i<sizeof(count)/sizeof(*count); i++)
-			if((int)count[i] != 0)
-				std::cout << "count[" << i << "]: "<< (int)count[i] << "\n";
-	}
-
-	/* struct size and alignment */
-	void TestAlignment(void)
-	{
-		std::cout << "\n* Struct Empty *\n"
-			<< "size of struct Empty: " << sizeof(struct Empty) << "\n"
-			<< "size of struct alignas(64) Empty64: " << sizeof(struct Empty64) << std::endl;
-		/* 6.2.8 Sizes */
-		std::cout << "\n* Struct Bar *\n";
-		std::cout << "machine word size: " << sizeof(size_t) << "\n"
-			"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
-			/* size:80 = 1 + 1 + 2 + 4 + 8*3 + 16*3 */
-			"bar:      " << sizeof(bar) << " (1 + 1 + 2 + 4 + 8*3 + 16*3)" << "\n"
-			"c:        " << sizeof(bar.c) << "\t" << alignof(bar.c) << "\t(" << &(bar) << ")" << "\n"
-			"b:        " << sizeof(bar.b) << "\t" << alignof(bar.b) << "\t(" << &(bar.b) << ")" << "\n"
-			"s:        " << sizeof(bar.s) << "\t" << alignof(bar.s) << "\t(" << &(bar.s) << ")" << "\n"
-			"i:        " << sizeof(bar.i) << "\t" << alignof(bar.i) << "\t(" << &(bar.i) << ")" << "\n"
-			"l:        " << sizeof(bar.l) << "\t" << alignof(bar.l) << "\t(" << &(bar.l) << ")" << "\n"
-			"ll:       " << sizeof(bar.ll) << "\t" << alignof(bar.ll) << "\t(" << &(bar.ll) << ")" << "\n"
-			"pi:       " << sizeof(bar.pi) << "\t" << alignof(bar.pi) << "\t(" << &(bar.pi) << ")" << "\n"
-			"d:        " << sizeof(bar.d) << "\t" << alignof(bar.d) << "\t(" << &(bar.d) << ")" << " <= compiler added 8 padding here\n"
-			"ld:       " << sizeof(bar.ld) << "\t" << alignof(bar.ld) << "\t(" << &(bar.ld) << ")" << "\n"
-			"arr[14]:  " << sizeof(bar.arr) << "\t" << alignof(bar.arr) << "\t(" << &(bar.arr) << ")" << " <= compiler added 2 more padding here\n";
-	}
-
-	/* struct with aligned members (alignas) */
-	void TestAlignment2(void)
-	{	
-		/* 6.2.8 Sizes */
-		std::cout << "\n* Struct Bar with alignment *\n";
-		std::cout << "machine word size: " << sizeof(size_t) << "\n"
-			"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
-			/* size:96 = 8*8 + 16*2 */
-			"bar2:     " << sizeof(bar2) << " (8*8 + 16*2)" << "\n"
-			"c:        " << sizeof(bar2.c) << "\t" << alignof(bar2.c) << "\t(" << &(bar2) << ")" << "\n"
-			"b:        " << sizeof(bar2.b) << "\t" << alignof(bar2.b) << "\t(" << &(bar2.b) << ")" << "\n"
-			"s:        " << sizeof(bar2.s) << "\t" << alignof(bar2.s) << "\t(" << &(bar2.s) << ")" << "\n"
-			"i:        " << sizeof(bar2.i) << "\t" << alignof(bar2.i) << "\t(" << &(bar2.i) << ")" << "\n"
-			"l:        " << sizeof(bar2.l) << "\t" << alignof(bar2.l) << "\t(" << &(bar2.l) << ")" << "\n"
-			"ll:       " << sizeof(bar2.ll) << "\t" << alignof(bar2.ll) << "\t(" << &(bar2.ll) << ")" << "\n"
-			"pi:       " << sizeof(bar2.pi) << "\t" << alignof(bar2.pi) << "\t(" << &(bar2.pi) << ")" << "\n"
-			"d:        " << sizeof(bar2.d) << "\t" << alignof(bar2.d) << "\t(" << &(bar2.d) << ")" << "\n"
-			"ld:       " << sizeof(bar2.ld) << "\t" << alignof(bar2.ld) << "\t(" << &(bar2.ld) << ")" << "\n"
-			"arr[14]:  " << sizeof(bar2.arr) << "\t" << alignof(bar2.arr) << "\t(" << &(bar2.arr) << ")" << "\n";
-	}
-
-	/* struct with packing */
-	void TestAlignment3(void)
-	{	
-		/* 6.2.8 Sizes */
-		std::cout << "\n* Struct Bar with packing *\n";
-		std::cout << "machine word size: " << sizeof(size_t) << "\n"
-			"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
-			/* size:70 = 1 + 1 + 2 + 4 + 8*4 + 16 + 14 */
-			"bar3:     " << sizeof(bar3) << " (1 + 1 + 2 + 4 + 8*4 + 16 + 14)" << "\n"
-			"c:        " << sizeof(bar3.c) << "\t" << alignof(bar3.c) << "\t(" << &(bar3) << ")" << "\n"
-			"b:        " << sizeof(bar3.b) << "\t" << alignof(bar3.b) << "\t(" << &(bar3.b) << ")" << "\n"
-			"s:        " << sizeof(bar3.s) << "\t" << alignof(bar3.s) << "\t(" << &(bar3.s) << ")" << "\n"
-			"i:        " << sizeof(bar3.i) << "\t" << alignof(bar3.i) << "\t(" << &(bar3.i) << ")" << "\n"
-			"l:        " << sizeof(bar3.l) << "\t" << alignof(bar3.l) << "\t(" << &(bar3.l) << ")" << "\n"
-			"ll:       " << sizeof(bar3.ll) << "\t" << alignof(bar3.ll) << "\t(" << &(bar3.ll) << ")" << "\n"
-			"pi:       " << sizeof(bar3.pi) << "\t" << alignof(bar3.pi) << "\t(" << &(bar3.pi) << ")" << "\n"
-			"d:        " << sizeof(bar3.d) << "\t" << alignof(bar3.d) << "\t(" << &(bar3.d) << ")" << "\n"
-			"ld:       " << sizeof(bar3.ld) << "\t" << alignof(bar3.ld) << "\t(" << &(bar3.ld) << ")" << "\n"
-			"arr[14]:  " << sizeof(bar3.arr) << "\t" << alignof(bar3.arr) << "\t(" << &(bar3.arr) << ")" << "\n";
-	}
-
-#if 0
-	/* UTF-8 encoding */
-	void TestStringLiteral(void)
-	{	
-		std::cout << "\n* UTF-8 *\n";
-		std::string s = "abcd 한글";	//u8"abcd 한글";
-
-		std::ostringstream result;
-		result << /*std::setw(8) << std::setfill('F') <<*/ std::hex << std::uppercase;
-		std::copy(s.begin(), s.end(), std::ostream_iterator<unsigned short>(result, " "));
-		std::cout << s << ":" << result.str() << std::endl;
-	}
-#endif
-
-	/* basic 'const' declarations */
-	void TestConst(void)
-	{
-		const int var1 = 10;
-		int var2 = 20;
-		int var3 = 30;
-
-		// 1. k1 is a pointer to a constant integer, 
-		//    you can't modify the pointed value.
-		const int * k1 = &var1;	
-		//var1 = 20;	// error
-		//*k1 = 20; 	// error
-		
-		// 2. this is an alternative syntax which does the same with k1
-		int const * k2 = &var1;	
-		
-		// 3. k3 is a constant pointer to a variable integer, 
-		//    you can't modify the value of your pointer, 
-		//    but you can modify the pointed value.
-		int * const k3 = &var2;
-		var2 *= 2;
-		*k3 *= 2;
-		//k3 = &var3;             // error. it's const pointer.
-		//int * const k3 = &var1; // error. var1 is constant integer and can't convert constant integer to integer.
-		
-		// 4. k4 is a constant pointer of a constant integer.
-		int const * const k4 = &var1;
-
-		// const return value
-		std::cout << "\n* const *\n"
-			<< "var1: " << var1 << "\n"
-			<< "var2: " << var2 << "\n"
-			<< func_with_const_return()[1]
-			<< func_with_const_return()[5] << std::endl;
-
-		// const member function
-		func_as_const_member();
-	}
-
-	void TestLRvalue(void)
-	{
-		/*  Lvalue is an expression that refers to an object. (somthing has identity, cannot be moved)
-		 *  Rvalue no longer exists after an expression. (movable, temporary)
-		 */
-
-		int x = 3;          // 3 is rvalue
-		const int y = x;
-		int z = x + y;      // x + y is rvalue
-		int arr[] = {1, 2}; // {1, 2} is rvalue
-
-
-		int* p = &x;    // unary '&' means 'address of', and it's rvalue (important!)
-		x = *p;         // unary '*' means 'contents of', and it's lvalue (important!)
-		// We can use address-of operator '&' only for lvalue since rvalue is movable.
-		//&x = 10;      // error, left operand of assignment must be lvalue.
-		//&(&x);        // error, '&' requires lvalue
-
-		p = &arr[0];    // &arr[0] is rvalue
-		*(p+1) = 10;    // okay, *(p+1) is lvalue.
-		&(*(p+1));      // okay, *(p+1) is lvalue.
-
-		std::cout << "\n* lvalue and rvalue *\n"
-			<< std::string("this is rvalue") << std::endl;	// return value of string() is rvalue
-
-		int i, j;
-		x = 1;
-		i = ++x;    // i = 2, x = 2
-		++x = i;    // okay, ++x is lvalue
-		&(++x);     // okay, ++x is lvalue
-
-		x = 1;
-		j = x++;    // j = 1, and x comes to 2 after the expression.
-		//x++ = j;  // error, x++ is rvalue
-		//&(x++);   // error, x++ is rvalue
-	}
+	const char *FuncWithConstReturn();
+	void FuncAsConstMember() const;
 };
 
-int main()
+/* 'const' return value in function */
+const char* Chapter6::FuncWithConstReturn()
+{ return "Some key"; }
+
+/* 'const' member function */
+void Chapter6::FuncAsConstMember() const
+{
+	// x = 1	// error
+	std::cout << x << "\n";
+}
+
+/* signed and unsigned */
+void Chapter6::TestSign()
+{
+	std::cout << "\n* Signed and Unsigned Characters *\n";
+	// 100 == 0110 0100, 1's complement: 1001 1011, 2's comlement: 1001 1100
+	// then, -100 == 1001 1100
+	signed char sc = -100;	
+	unsigned char uc = sc;	// 1001 1100 == 156 (unsigned), -100 (signed)
+	std::cout << 
+		"sc: " << (int)sc << "\n"
+		"uc: " << (int)uc << "\n";
+
+	char count[256] = {0,};
+	char *cp = count;
+	std::cout << "address of count[0]: " << &(count) << "\n";
+	std::cout << "address of count[-100]: " << (void*)(cp-100) << "\n";
+	//std::cout << (int)*(cp-120) << "\n";
+	//std::cout << (int)*(cp+136) << "\n";
+	std::cout << "count[-100]: " << (int)count[-100] << "\n";
+	std::cout << "count[156]: " << (int)count[156] << "\n";
+	std::cout << "increasement of count[-100]: " << ""; 
+	++count[sc];	// undefined behavior
+	std::cout << (int)*(cp-100) << " (undefined behavior)\n";
+	std::cout << "increasement of count[156]: " << "";
+	++count[uc];
+	std::cout << (int)*(cp+156) << "\n";
+
+	for(int i; i<sizeof(count)/sizeof(*count); i++)
+		if((int)count[i] != 0)
+			std::cout << "count[" << i << "]: "<< (int)count[i] << "\n";
+}
+
+/* struct size and alignment */
+void Chapter6::TestAlignment()
+{
+	std::cout << "\n* Struct Empty *\n"
+		<< "size of struct Empty: " << sizeof(struct Empty) << "\n"
+		<< "size of struct alignas(64) Empty64: " << sizeof(struct Empty64) << std::endl;
+	/* 6.2.8 Sizes */
+	std::cout << "\n* Struct Bar *\n";
+	std::cout << "machine word size: " << sizeof(size_t) << "\n"
+		"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
+		/* size:80 = 1 + 1 + 2 + 4 + 8*3 + 16*3 */
+		"bar:      " << sizeof(bar) << " (1 + 1 + 2 + 4 + 8*3 + 16*3)" << "\n"
+		"c:        " << sizeof(bar.c) << "\t" << alignof(bar.c) << "\t(" << &(bar) << ")" << "\n"
+		"b:        " << sizeof(bar.b) << "\t" << alignof(bar.b) << "\t(" << &(bar.b) << ")" << "\n"
+		"s:        " << sizeof(bar.s) << "\t" << alignof(bar.s) << "\t(" << &(bar.s) << ")" << "\n"
+		"i:        " << sizeof(bar.i) << "\t" << alignof(bar.i) << "\t(" << &(bar.i) << ")" << "\n"
+		"l:        " << sizeof(bar.l) << "\t" << alignof(bar.l) << "\t(" << &(bar.l) << ")" << "\n"
+		"ll:       " << sizeof(bar.ll) << "\t" << alignof(bar.ll) << "\t(" << &(bar.ll) << ")" << "\n"
+		"pi:       " << sizeof(bar.pi) << "\t" << alignof(bar.pi) << "\t(" << &(bar.pi) << ")" << "\n"
+		"d:        " << sizeof(bar.d) << "\t" << alignof(bar.d) << "\t(" << &(bar.d) << ")" << " <= compiler added 8 padding here\n"
+		"ld:       " << sizeof(bar.ld) << "\t" << alignof(bar.ld) << "\t(" << &(bar.ld) << ")" << "\n"
+		"arr[14]:  " << sizeof(bar.arr) << "\t" << alignof(bar.arr) << "\t(" << &(bar.arr) << ")" << " <= compiler added 2 more padding here\n";
+}
+
+/* struct with aligned members (alignas) */
+void Chapter6::TestAlignment2()
+{	
+	/* 6.2.8 Sizes */
+	std::cout << "\n* Struct Bar with alignment *\n";
+	std::cout << "machine word size: " << sizeof(size_t) << "\n"
+		"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
+		/* size:96 = 8*8 + 16*2 */
+		"bar2:     " << sizeof(bar2) << " (8*8 + 16*2)" << "\n"
+		"c:        " << sizeof(bar2.c) << "\t" << alignof(bar2.c) << "\t(" << &(bar2) << ")" << "\n"
+		"b:        " << sizeof(bar2.b) << "\t" << alignof(bar2.b) << "\t(" << &(bar2.b) << ")" << "\n"
+		"s:        " << sizeof(bar2.s) << "\t" << alignof(bar2.s) << "\t(" << &(bar2.s) << ")" << "\n"
+		"i:        " << sizeof(bar2.i) << "\t" << alignof(bar2.i) << "\t(" << &(bar2.i) << ")" << "\n"
+		"l:        " << sizeof(bar2.l) << "\t" << alignof(bar2.l) << "\t(" << &(bar2.l) << ")" << "\n"
+		"ll:       " << sizeof(bar2.ll) << "\t" << alignof(bar2.ll) << "\t(" << &(bar2.ll) << ")" << "\n"
+		"pi:       " << sizeof(bar2.pi) << "\t" << alignof(bar2.pi) << "\t(" << &(bar2.pi) << ")" << "\n"
+		"d:        " << sizeof(bar2.d) << "\t" << alignof(bar2.d) << "\t(" << &(bar2.d) << ")" << "\n"
+		"ld:       " << sizeof(bar2.ld) << "\t" << alignof(bar2.ld) << "\t(" << &(bar2.ld) << ")" << "\n"
+		"arr[14]:  " << sizeof(bar2.arr) << "\t" << alignof(bar2.arr) << "\t(" << &(bar2.arr) << ")" << "\n";
+}
+
+/* struct with packing */
+void Chapter6::TestAlignment3()
+{	
+	/* 6.2.8 Sizes */
+	std::cout << "\n* Struct Bar with packing *\n";
+	std::cout << "machine word size: " << sizeof(size_t) << "\n"
+		"MEMBER    " << "SIZE\t" << "ALIGN\t" << "ADDRESS" << "\n"
+		/* size:70 = 1 + 1 + 2 + 4 + 8*4 + 16 + 14 */
+		"bar3:     " << sizeof(bar3) << " (1 + 1 + 2 + 4 + 8*4 + 16 + 14)" << "\n"
+		"c:        " << sizeof(bar3.c) << "\t" << alignof(bar3.c) << "\t(" << &(bar3) << ")" << "\n"
+		"b:        " << sizeof(bar3.b) << "\t" << alignof(bar3.b) << "\t(" << &(bar3.b) << ")" << "\n"
+		"s:        " << sizeof(bar3.s) << "\t" << alignof(bar3.s) << "\t(" << &(bar3.s) << ")" << "\n"
+		"i:        " << sizeof(bar3.i) << "\t" << alignof(bar3.i) << "\t(" << &(bar3.i) << ")" << "\n"
+		"l:        " << sizeof(bar3.l) << "\t" << alignof(bar3.l) << "\t(" << &(bar3.l) << ")" << "\n"
+		"ll:       " << sizeof(bar3.ll) << "\t" << alignof(bar3.ll) << "\t(" << &(bar3.ll) << ")" << "\n"
+		"pi:       " << sizeof(bar3.pi) << "\t" << alignof(bar3.pi) << "\t(" << &(bar3.pi) << ")" << "\n"
+		"d:        " << sizeof(bar3.d) << "\t" << alignof(bar3.d) << "\t(" << &(bar3.d) << ")" << "\n"
+		"ld:       " << sizeof(bar3.ld) << "\t" << alignof(bar3.ld) << "\t(" << &(bar3.ld) << ")" << "\n"
+		"arr[14]:  " << sizeof(bar3.arr) << "\t" << alignof(bar3.arr) << "\t(" << &(bar3.arr) << ")" << "\n";
+}
+
+#if 0
+/* UTF-8 encoding */
+void Chapter6::TestStringLiteral()
+{	
+	std::cout << "\n* UTF-8 *\n";
+	std::string s = "abcd 한글";	//u8"abcd 한글";
+
+	std::ostringstream result;
+	result << /*std::setw(8) << std::setfill('F') <<*/ std::hex << std::uppercase;
+	std::copy(s.begin(), s.end(), std::ostream_iterator<unsigned short>(result, " "));
+	std::cout << s << ":" << result.str() << std::endl;
+}
+#endif
+
+/* 'const' declarations */
+void Chapter6::TestConst()
+{
+	const int var1 = 10;
+	int var2 = 20;
+	int var3 = 30;
+
+	// 1. k1 is a pointer to a constant integer, 
+	//    you can't modify the pointed value.
+	const int * k1 = &var1;	
+	//var1 = 20;	// error
+	//*k1 = 20; 	// error
+	
+	// 2. this is an alternative syntax which does the same with k1
+	int const * k2 = &var1;	
+	
+	// 3. k3 is a constant pointer to a variable integer, 
+	//    you can't modify the value of your pointer, 
+	//    but you can modify the pointed value.
+	int * const k3 = &var2;
+	var2 *= 2;
+	*k3 *= 2;
+	//k3 = &var3;             // error. it's const pointer.
+	//int * const k3 = &var1; // error. var1 is constant integer and can't convert constant integer to integer.
+	
+	// 4. k4 is a constant pointer of a constant integer.
+	int const * const k4 = &var1;
+
+	// const return value
+	std::cout << "\n* const *\n"
+		<< "var1: " << var1 << "\n"
+		<< "var2: " << var2 << "\n"
+		<< FuncWithConstReturn()[1]
+		<< FuncWithConstReturn()[5] << std::endl;
+
+	// const member function
+	FuncAsConstMember();
+	
+	//TODO: mutable and const_cast
+}
+
+/* Lvalue and Rvalue */
+void Chapter6::TestLRvalue()
+{
+	/*  Lvalue is an expression that refers to an object. (somthing has identity, cannot be moved)
+	 *  Rvalue no longer exists after an expression. (movable, temporary)
+	 */
+
+	int x = 3;          // 3 is rvalue
+	const int y = x;
+	int z = x + y;      // x + y is rvalue
+	int arr[] = {1, 2}; // {1, 2} is rvalue
+
+
+	int* p = &x;    // unary '&' means 'address of', and it's rvalue (important!)
+	x = *p;         // unary '*' means 'contents of', and it's lvalue (important!)
+	// We can use address-of operator '&' only for lvalue since rvalue is movable.
+	//&x = 10;      // error, left operand of assignment must be lvalue.
+	//&(&x);        // error, '&' requires lvalue
+
+	p = &arr[0];    // &arr[0] is rvalue
+	*(p+1) = 10;    // okay, *(p+1) is lvalue.
+	&(*(p+1));      // okay, *(p+1) is lvalue.
+
+	std::cout << "\n* lvalue and rvalue *\n"
+		<< std::string("this is rvalue") << std::endl;	// return value of string() is rvalue
+
+	int i, j;
+	x = 1;
+	i = ++x;    // i = 2, x = 2
+	++x = i;    // okay, ++x is lvalue
+	&(++x);     // okay, ++x is lvalue
+
+	x = 1;
+	j = x++;    // j = 1, and x comes to 2 after the expression.
+	//x++ = j;  // error, x++ is rvalue
+	//&(x++);   // error, x++ is rvalue
+}
+
+int main(int argc, char *argv[])
 {
 	std::unique_ptr<Chapter6> ch6 = std::make_unique<Chapter6>();
 
