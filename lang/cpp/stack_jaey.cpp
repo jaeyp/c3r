82dabfda947204383b49de6e64ca771b55860cc4
jaeyp <jp.inseoul@gmail.com>
2019-08-18 02:43:52 +0000

    added isFull() and isEmpty()

--- lang/cpp/stack_jaey.cpp
+++ lang/cpp/stack_jaey.cpp
@@ -5,40 +5,59 @@ public:
 	Stack(int size) : size{size}, top{0} {
 		stack = new int[size];
 	}
+
 	~Stack(){
 		delete stack;
 	}
+
 	void push(int data) {
-		top = top + 1;
-		stack[top] = data;
+		if(isFull()) {
+			std::cout << "stack is full" << std::endl;
+			return;
+		}
+		stack[top++] = data;
 	}
+
 	int pop() {
-		int pos = top;
-		top = top - 1;
-		return stack[pos]; 
+		if(isEmpty()) {
+			std::cout << "stack is empty" << std::endl;
+			return -1;
+		}
+		return stack[--top]; 
 	}
+
+	int peek() {
+		if(isEmpty()) {
+			std::cout << "stack is empty" << std::endl;
+			return -1;
+		}
+		return stack[top-1];
+	}
+
 private:
 	int* stack;
 	int size;
 	int top;
+
+	bool isFull() {
+		return top == size;
+	}
+
 	bool isEmpty() {
 		return top == 0;
-	}
-	bool getSize() {
-		return size;
 	}
 };
 
 
 int main(int argc, char** argv) {
-	Stack s = Stack(10);
-	s.push(1);
-	s.push(2);
-	s.push(3);
-	s.push(4);
+	Stack s(8);
+	for(int i=0; i<10; i++) {
+		s.push(i);
+	}
 
-	std::cout << s.pop() << std::endl;
-	std::cout << s.pop() << std::endl;
-	std::cout << s.pop() << std::endl;
-	std::cout << s.pop() << std::endl;
+	std::cout << "peek: " << s.peek() << std::endl;
+
+	for(int i=0; i<10; i++) {
+		std::cout << "pop: " << s.pop() << std::endl;
+	}
 }
