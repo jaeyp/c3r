a239faf49b230542e0fe152a2f02f393c123dacc
jaeyp <jp.inseoul@gmail.com>
2019-08-18 02:33:02 +0000

    added three ways of defining component

--- web/react/practice/src/App.js
+++ web/react/practice/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './component/Counter';
 
+// Define Component way-1
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+	  <Counter />
       </header>
     </div>
   );
 }
+
+// Define Component way-2
+/*
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+	  	<Counter />
+      </header>
+    </div>
+  );
+};
+*/
+
+// Define Component way-3
+/*
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+				<Counter />
+				</header>
+			</div>
+		);
+	}
+}
+*/
 
 export default App;
