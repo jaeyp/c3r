e29e67ba568079baeb84ec4e4dabf86ee8266fd2
nowhh01 <kyeongsigi@gmail.com>
2019-09-13 02:49:30 +0000

    Updated bubble and comb sorts

--- lang/cpp/bubble_sort_jayden.cpp
+++ lang/cpp/bubble_sort_jayden.cpp
@@ -1,60 +1,124 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <array>
 
 // average: O(n * n)
 
-template<typename T>
-void BubbleSort(T data[], int size)
+template<typename Comparable>
+void BubbleSort(const int size, Comparable* const outData)
 {
-	for(int i = 0; i < size-1; i++)
+	for(int i = 0; i < size - 1; i++)
 	{
-		for(int j = size-1; j > i; j--)
+		for(int j = size - 1; j > i; j--)
 		{
-			if(data[j] < data[j-1])
+			if(*(outData + j) < *(outData + j - 1))
 			{
-				std::swap(data[j], data[j-1]);
+				std::swap(*(outData + j), *(outData + j - 1));
 			}
 		}
 	}
 }
 
-template<typename T>
-void BubbleSort2(T data[], int size)
+template<typename Comparable>
+void BubbleSort2(const int size, Comparable* const outData)  // better
 {
 	bool again{true};
-	for(int i = 0; i < size-1 && again; i++)
+	for(int i = 0; i < size - 1 && again; i++)
 	{
 		again = false;
-		for(int j = size-1; j > i; j--)
+		for(int j = size - 1; j > i; j--)
 		{
-			if(data[j] < data[j-1])
+			if(*(outData + j) < *(outData + j - 1))
 			{
-				std::swap(data[j], data[j-1]);
+				std::swap(*(outData + j), *(outData + j - 1));
 				again = true;    // to check if swaped 
 			}
 		}
 	}
 }
 
+template<typename Comparable, size_t N>
+void BubbleSort(std::array<Comparable, N>* const outData) // for std::array
+{
+	bool again{ true };
+	for(size_t i = 0; i < N - 1 && again; i++)
+	{
+		again = false;
+		for(size_t j = N - 1; j > i; j--)
+		{
+			if(outData->at(j) < outData->at(j - 1))
+			{
+				std::swap(outData->at(j), outData->at(j - 1));
+				again = true;
+			}
+		}
+	}
+}
+
+template<typename Comparable>
+void BubbleSort(std::vector<Comparable>* const outData) // for std::vector
+{
+	size_t size = outData->size();
+	bool again{ true };
+	
+	for(size_t i = 0; i < size - 1 && again; i++)
+	{
+		again = false;
+		for(int j = size - 1; j > i; j--)
+		{
+			if(outData->at(j) < outData->at(j - 1))
+			{
+				std::swap(outData->at(j), outData->at(j - 1));
+				again = true;
+			}
+		}
+	}
+}
+
+template<typename T>
+void Swap(T* const outFirst, T* const outSecond) // same as std::swap()
+{
+	T tmp{ std::move(*outFirst) };
+	*outFirst = std::move(*outSecond);
+	*outSecond = std::move(tmp);
+}
+
 int main()
 {
 	int a1[7]{ 8, 6, 4, 4, 1, 9, 2 };
-	int a2[7];
-	std::copy(a1, a1+7, a2);
+	int a2[7]{};
+	std::copy(a1, a1 + 7, a2);
+	std::array<int, 7> a3{};
+	std::copy(a1, a1 + 7, a3.begin());
+	std::vector<int> a4{a1, a1 + 7};
 
-	BubbleSort(a1, 7);
-	BubbleSort2(a2, 7);
+	BubbleSort(7, a1);
+	BubbleSort2(7, a2);
+	BubbleSort(&a3);
+	BubbleSort(&a4);
 
-	for(int i = 0; i < 7; i++)
+	for(const auto& x : a1)
 	{
-		std::cout << a1[i] << ' ';
+		std::cout << x << ' ';
 	}
 	std::cout << '\n';
 
-	for(int i = 0; i < 7; i++)
+	for(const auto& x : a2)
 	{
-		std::cout << a2[i] << ' ';
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+
+	for(const auto& x : a3)
+	{
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+
+	for(const auto& x : a4)
+	{
+		std::cout << x << ' ';
 	}
 	std::cout << '\n';
 }
--- lang/cpp/comb_sort_jayden.cpp
+++ lang/cpp/comb_sort_jayden.cpp
@@ -1,49 +1,147 @@
 #include <iostream>
+#include <array>
+#include <vector>
 
 // O(n log n)
 // worst case: O(n * n) due to phase 2
 // comparable to the performance of quicksort
 
-template<typename T>
-void CombSort(T data[], const int size)   // improved BubbleSort2()
+template<typename Comparable>
+void CombSort(const int size, Comparable* const outData)   // improved BubbleSort2()
 {
 	int i, j, k;
-	int step = size;
+	int step{ size };
+
 	while((step = int(step / 1.3)) > 1)   // phase 1
 	{
-		for(j = size-1; j >= step; j--)
+		for(j = size - 1; j >= step; j--)
 		{
-			k = j-step;
-			if(data[j] < data[k])
+			k = j - step;
+			if(*(outData + j) < *(outData + k))
 			{
-				std::swap(data[j], data[k]);
+				std::swap(*(outData + j), *(outData + k));
 			}
 		}
 	}
 
 	bool again{ true };
-	for(i = 0; i < size-1 && again; i++)  // phase 2
+	for(i = 0; i < size - 1 && again; i++)  // phase 2
 	{
 		again = false;
-		for(j = size-1; j > i; j--)
+		for(j = size - 1; j > i; j--)
 		{
-			if(data[j] < data[j-1])
+			if(*(outData + j) < *(outData + j - 1))
 			{
-				std::swap(data[j], data[j-1]);
+				std::swap(*(outData + j), *(outData + j - 1));
 				again = true;
 			}
 		}
 	}
 }
 
+template<typename Comparable, size_t N>
+void CombSort(std::array<Comparable, N>* const outData) // for std::array
+{
+	size_t i, j, k;
+	size_t step{ N };
+
+	while((step = size_t(step / 1.3)) > 1)
+	{
+		for(j = N - 1; j > step; j--)
+		{
+			k = j - step;
+
+			if(outData->at(j) < outData->at(k))
+			{
+				std::swap(outData->at(j), outData->at(k));
+			}
+		}
+	}
+
+	bool again{ true };
+	for(i = 0; i < N - 1 && again; i++)
+	{
+		again = false;
+		for(j = N - 1; j > i; j--)
+		{
+			if(outData->at(j) < outData->at(j - 1))
+			{
+				std::swap(outData->at(j), outData->at(j - 1));
+				again = true;
+			}
+		}
+	}
+}
+
+template<typename Comparable>
+void CombSort(std::vector<Comparable>* const outData) // for std::vector
+{
+	size_t i, j, k;
+	size_t size{ outData->size() };
+	size_t step{ size };
+
+	while((step = size_t(step / 1.3)) > 1)
+	{
+		for(j = size - 1; j > step; j--)
+		{
+			k = j - step;
+
+			if(outData->at(j) < outData->at(k))
+			{
+				std::swap(outData->at(j), outData->at(k));
+			}
+		}
+	}
+
+	bool again{ true };
+	for(i = 0; i < size - 1 && again; i++)
+	{
+		again = false;
+		for(j = size - 1; j > i; j--)
+		{
+			if(outData->at(j) < outData->at(j - 1))
+			{
+				std::swap(outData->at(j), outData->at(j - 1));
+				again - true;
+			}
+		}
+	}
+}
+
+template<typename T>
+void Swap(T* const outFirst, T* const outSecond) // same as std::swap()
+{
+	T tmp{ std::move(*outFirst) };
+	*outFirst = std::move(*outSecond);
+	*outSecond = std::move(tmp);
+}
+
 int main()
 {
 	int a1[7]{ 8, 6, 4, 4, 1, 9, 2 };
-	CombSort(a1, 7);
+	std::array<int, 7> a2{};
+	std::copy(a1, a1 + 7, a2.begin());
+	std::vector<int> a3{ a1, a1 + 7 };
 
-	for(int i = 0; i < 7; i++)
+	CombSort(7, a1);
+	CombSort(&a2);
+	CombSort(&a3);
+
+	for(const auto& x : a1)
 	{
-		std::cout << a1[i] << ' ';
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+
+	for(const auto& x : a2)
+	{
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+	
+	for(const auto& x : a3)
+	{
+		std::cout << x << ' ';
 	}
 	std::cout << '\n';
 }
